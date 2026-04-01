a160c76915aa79eaa0eb0897134a6ae9ebe6fe92
Artur <artur.bernardes@edu.ufes.br>
2026-04-01 15:44:27 -0300

    fix: comment

--- Hello.java
+++ Hello.java
@@ -1,4 +1,5 @@
-#Classe Hello para gerar objetos ''Hello's'' com método de printar 'Hello World'"
+// Classe Hello para gerar objetos ''Hello's'' com método de printar 'Hello World'"
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello " + args[0] + "!");
