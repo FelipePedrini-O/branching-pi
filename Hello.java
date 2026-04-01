d46789a42b890c9041885c18b3875be56eb6c18f
lewislf <luizfernandosouzacc@gmail.com>
2026-04-01 15:34:39 -0300

    doc: adding new comments

--- Hello.java
+++ Hello.java
@@ -1,3 +1,4 @@
+#Classe Hello para gerar objetos ''Hello's'' com método de printar 'Hello World'"
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
