14e8c59c095e31da3cfb6a024b3eaffd92130f3d
FelipePedrini-O <buracoba@gmail.com>
2026-04-08 16:36:44 -0300

    feat: headers

--- AnxiousBoy/src/main/java/br/ufes/buracobonicos/App.java
+++ AnxiousBoy/src/main/java/br/ufes/buracobonicos/App.java
@@ -1,14 +1,36 @@
 package br.ufes.buracobonicos;
 
-import org.apache;
-/**
- * Hello world!
- *
- */
+import java.io.FileReader;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );   
+        String path = "/home/fpoliveira/branching-pi/AnxiousBoy/female-and-male-life-expectancy-at-birth-in-years/female-and-male-life-expectancy-at-birth-in-years.csv"; 
+        Reader in = new FileReader(path);
+        Iterable<CSVRecord> records_inp = CSVFormat.RFC4180.builder()
+        .setHeader()
+        .setSkipHeaderRecord(true)
+        .build()
+        .parse(in);
+
+        for (CSVRecord record : records_inp) {
+            String entity = record.get("Entity");
+            String code = record.get("Code");
+            String year = record.get("Year");
+            String men = record.get("Men");
+            String women = record.get("Women");
+            System.out.println(entity + code + year + men + women);
+        }
+
+        /* Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(in);
+        for (CSVRecord record : records) {
+            String columnOne = record.get(0);
+            String columnTwo = record.get(1);
+        }   */
     }
 }
